08020a7715da24a45247971374180073de22d39e
leonlin14 <leonlin14@gmail.com>
2014-11-30 14:14:14 +0800

    implement create, close #2

--- routes/posts.js
+++ routes/posts.js
@@ -1,17 +1,26 @@
-var post = 
-[{
-	"title": "您好",
-	"content": "今天星期日"
-},
-{
-	"title": "Hello",
-	"content": "Today is Sunday"
-}];
-
-exports.list = function (req, res) {
-	res.send(post);
+exports.list = function(req, res){
+	var model = req.app.db.model.Post;
+ 	
+   	model
+   		.find({})
+   		.exec(function(err, posts) {
+ 		  	res.send({
+ 		  		posts: posts
+ 		  	});
+ 		  	res.end();
+   		});
 };
 
-exports.create = function (req, res) {
+exports.create = function(req, res){
+	var model = req.app.db.model.Post;
+	var title = req.query.title;
+	var content = req.query.content;
 
+	var post = new model({
+		title: title,
+		content: content
+	});
+	post.save();
+
+	res.send({status: 'OK'});
 };
