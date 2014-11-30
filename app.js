e2d480aa68c608d03d0e379317b88262faf5f08e
leonlin14 <leonlin14@gmail.com>
2014-11-30 13:51:46 +0800

    add post API module, #4

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var posts = require('./routes/posts');
 
 var app = express();
 
@@ -56,5 +57,7 @@ app.use(function(err, req, res, next) {
     });
 });
 
+app.get('/1/post', posts.list);
+app.post('/1/post', posts.create);
 
 module.exports = app;
