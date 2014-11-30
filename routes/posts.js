67438b47c52f0509d7167467036dd8398e23b35e
leonlin14 <leonlin14@gmail.com>
2014-11-30 14:01:44 +0800

    add post data, #4

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,15 @@
-exports.list = function (req, res) {
+var post = 
+[{
+	"title": "您好",
+	"content": "今天星期日"
+},
+{
+	"title": "Hello",
+	"content": "Today is Sunday"
+}];
 
+exports.list = function (req, res) {
+	res.send(post);
 };
 
 exports.create = function (req, res) {
