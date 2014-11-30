5a6bb6b40c0b60da9d4ffb56790e0ac39de9f834
leonlin14 <leonlin14@gmail.com>
2014-11-30 14:22:32 +0800

    implement state: validation, #5

--- routes/posts.js
+++ routes/posts.js
@@ -19,8 +19,21 @@ exports.create = function(req, res){
 	var title = req.query.title;
 	var content = req.query.content;
 
+	workflow.outcome = {
+		success: false,
+		errfor: {}
+	};
+
 	workflow.on('validation', function() {
-		if (hasError) workflow.emit('response');
+		title = req.body.title;
+		content = req.body.content;
+
+		if(title.length === 0)
+			workflow.outcome.errfor.subject = '這是必填欄位';
+		if(content.length === 0)
+			workflow.outcome.errfor.subject = '這是必填欄位';
+		if(Object.keys(workflow.outcome.errfor).length !== 0)
+			return workflow.emit('response');
 		workflow.emit('savePost');
 	});
 
